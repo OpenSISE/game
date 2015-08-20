80f1ce72963b037f858164bd9b95261e3bf4394d
Randy <randypriv@gmail.com>
2015-08-20 18:34:15 +0800

    change to webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('global','global.js');
+
+module.exports = {
+  entry: {
+    Home: './public/dev/js/home.js',
+    Sign: './public/dev/js/sign.js',
+    Profile: './public/dev/js/profile.js'
+  },
+  output: {
+    path: 'public/dev/build',
+    publicPath: 'build/',
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {test: /\.css$/, loader: 'style!css'},
+      {test: /\.scss$/, loader: 'style!css!sass'},
+      {test: /\.png$/, loader: 'url-loader?limit=8192'}
+    ]
+  },
+  resolve: {
+    extensions: ['','.js']
+  },
+  plugins: [commonsPlugin]
+}
